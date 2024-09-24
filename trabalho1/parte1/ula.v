714d616c6d01e73655c7288c89ec4950281a9c28
Pedro Amaral <amaral1@ufpr.br>
2024-09-24 20:35:12 -0300

    Update ula.v

--- trabalho1/parte1/ula.v
+++ trabalho1/parte1/ula.v
@@ -1,14 +1,12 @@
 module ula (
   input wire [7:0] ra,
   input wire [7:0] rb,
-  input wire [3:0] opcode,
-  output wire [7:0] result,
-  output wire carry_out       
+  input wire [2:0] opcode,
+  output wire [7:0] result      
 );
 
   // Variável para cada resultado das operações da ULA
   wire [7:0] not_result, and_result, or_result, xor_result, add_result, sub_result, slr_result, srr_result;
-  wire slr_carry_out, srr_carry_out;
 
   // Todas as operações da ULA
   assign not_result = ~ra;
@@ -19,15 +17,7 @@ module ula (
   assign sub_result = ra - rb;
   assign slr_result = ra << rb;
   assign srr_result = ra >> rb;
-
-  // Carry out dos shifts é o bit mais "significativo"
-  assign slr_carry_out = ra[7];
-  assign srr_carry_out = ra[0];
   
   mux8 mux_saida(not_result, and_result, or_result, xor_result, add_result, sub_result, slr_result, srr_result, opcode, result);
-
-  // Seleção do carry_out baseado no opcode
-  assign carry_out = (opcode == 4'b1110) ? slr_carry_out :
-                     (opcode == 4'b1111) ? srr_carry_out :
-                     1'b0;			
+		
 endmodule
