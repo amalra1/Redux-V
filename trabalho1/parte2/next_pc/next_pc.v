9e47e0e9c680e4cf8f7c0bd2a010b91178076a16
schueda <a.schuedamenezes@gmail.com>
2024-10-25 12:20:18 -0300

    Reestrutura projeto

--- trabalho1/parte2/next_pc/next_pc.v
+++
@@ -1,9 +0,0 @@
-module next_pc (
-    input wire jmx, bmx,
-    input wire[7:0] curPC, imm, b,
-    output wire[7:0] pc
-);
-    assign pc = (bmx == 1)? b :
-                (jmx == 1)? curPC + imm:
-                curPC + 8'd1;
-endmodule
