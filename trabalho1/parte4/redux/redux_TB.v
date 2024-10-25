f1893e1ba76faef3f1b2aaa9c5062467a45f9186
schueda <a.schuedamenezes@gmail.com>
2024-10-25 11:06:57 -0300

    Corrige execução do algoritmo 2

--- trabalho1/parte4/redux/redux_TB.v
+++ trabalho1/parte4/redux/redux_TB.v
@@ -7,17 +7,21 @@ redux RED(.clk(clk));
 initial begin 
     $dumpfile("redux.vcd");
     $dumpvars(5, redux_TB);
+    $dumpvars(0, RED.BRE.regs[0]);
+    $dumpvars(0, RED.BRE.regs[1]);
+    $dumpvars(0, RED.BRE.regs[2]);
+    $dumpvars(0, RED.BRE.regs[3]);
 end
 
 initial begin: clock
     clk = 0;
-    for (integer i = 0; i < 200; i++) begin
+    for (integer i = 0; i < 1000; i++) begin
         #10;
         clk = ~clk;
     end
     
     for (reg[7:0] j = 0; j < 255; j++) begin
-        $display("data[%d] = %d", j, RED.MED.memoria[j]);
+        $display("data[%d] = %d", j, $signed(RED.MED.memoria[j]));
     end
 end
     
